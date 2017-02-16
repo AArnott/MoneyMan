5f83b5f6a4d14887e5c465c8e2a79cb9253a1883
Andrew Arnott <andrewarnott@gmail.com>
2017-02-16 08:35:08 -0800

    Add WPF app

---
+++ src/MoneyMan.WPF/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the Ms-PL license. See LICENSE.txt file in the project root for full license information.
+
+namespace MoneyMan.WPF
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Data;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
