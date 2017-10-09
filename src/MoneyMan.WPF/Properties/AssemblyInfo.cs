47c311a241e1326be8c999546836b68ae4dbdddb
Andrew Arnott <andrewarnott@gmail.com>
2017-10-08 20:45:28 -0700

    Fix StyleCop warning

--- src/MoneyMan.WPF/Properties/AssemblyInfo.cs
+++ src/MoneyMan.WPF/Properties/AssemblyInfo.cs
@@ -34,9 +34,5 @@ using System.Windows;
 
 #pragma warning disable SA1115 // Parameter must follow comma
 [assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, // where theme specific resource dictionaries are located
-                                     // (used if a resource is not found in the page,
-                                     // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly)] // where the generic resource dictionary is located
-                                                // (used if a resource is not found in the page,
-                                                // app, or any theme specific resource dictionaries)
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly)]
