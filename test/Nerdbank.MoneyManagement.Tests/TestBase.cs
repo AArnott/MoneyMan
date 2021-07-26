55a847f451f56fcfa7eb99d0219b6607c60c174a
Andrew Arnott <andrewarnott@live.com>
2021-07-26 09:19:54 -0600

    Refactor test base classes

--- test/Nerdbank.MoneyManagement.Tests/TestBase.cs
+++ test/Nerdbank.MoneyManagement.Tests/TestBase.cs
@@ -4,12 +4,33 @@
 using System;
 using Xunit.Abstractions;
 
-public abstract class TestBase
+public abstract class TestBase : IDisposable
 {
+	private bool disposed;
+
 	public TestBase(ITestOutputHelper logger)
 	{
 		this.Logger = logger ?? throw new ArgumentNullException(nameof(logger));
 	}
 
 	protected ITestOutputHelper Logger { get; }
+
+	public void Dispose()
+	{
+		// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+		this.Dispose(disposing: true);
+		GC.SuppressFinalize(this);
+	}
+
+	protected virtual void Dispose(bool disposing)
+	{
+		if (!this.disposed)
+		{
+			if (disposing)
+			{
+			}
+
+			this.disposed = true;
+		}
+	}
 }
