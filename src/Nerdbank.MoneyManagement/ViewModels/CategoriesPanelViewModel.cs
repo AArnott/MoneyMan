1ed3eb51414fcfb94be22efdec59a5c50eefb527
Andrew Arnott <andrewarnott@live.com>
2021-08-08 20:01:07 -0600

    Add tests for the categories view model

--- src/Nerdbank.MoneyManagement/ViewModels/CategoriesPanelViewModel.cs
+++ src/Nerdbank.MoneyManagement/ViewModels/CategoriesPanelViewModel.cs
@@ -3,13 +3,17 @@
 
 namespace Nerdbank.MoneyManagement.ViewModels
 {
+	using System;
 	using System.Collections.ObjectModel;
+	using System.ComponentModel;
+	using System.Threading;
+	using System.Threading.Tasks;
 	using System.Windows.Input;
 	using PCLCommandBase;
 
 	public class CategoriesPanelViewModel : BindableBase
 	{
-		private ObservableCollection<CategoryViewModel> categories = new ObservableCollection<CategoryViewModel>();
+		private readonly ObservableCollection<CategoryViewModel> categories = new ObservableCollection<CategoryViewModel>();
 		private CategoryViewModel? selectedCategory;
 
 		public CategoriesPanelViewModel()
@@ -22,16 +26,58 @@ namespace Nerdbank.MoneyManagement.ViewModels
 
 		public ICommand DeleteCommand { get; }
 
-		public ObservableCollection<CategoryViewModel> Categories
-		{
-			get => this.categories;
-			set => this.SetProperty(ref this.categories, value);
-		}
+		public ObservableCollection<CategoryViewModel> Categories => this.categories;
 
 		public CategoryViewModel? SelectedCategory
 		{
 			get => this.selectedCategory;
 			set => this.SetProperty(ref this.selectedCategory, value);
+		}
+
+		private class AddCategoryCommand : CommandBase
+		{
+			private readonly CategoriesPanelViewModel viewModel;
+
+			public AddCategoryCommand(CategoriesPanelViewModel viewModel)
+			{
+				this.viewModel = viewModel;
+			}
+
+			protected override Task ExecuteCoreAsync(object parameter, CancellationToken cancellationToken)
+			{
+				CategoryViewModel newCategoryViewModel = new();
+				this.viewModel.Categories.Add(newCategoryViewModel);
+				this.viewModel.SelectedCategory = newCategoryViewModel;
+				return Task.CompletedTask;
+			}
+		}
+
+		private class DeleteCategoryCommand : CommandBase
+		{
+			private readonly CategoriesPanelViewModel viewModel;
+
+			public DeleteCategoryCommand(CategoriesPanelViewModel viewModel)
+			{
+				this.viewModel = viewModel;
+				viewModel.PropertyChanged += this.ViewModel_PropertyChanged;
+			}
+
+			public override bool CanExecute(object parameter) => base.CanExecute(parameter) && this.viewModel.SelectedCategory is object;
+
+			protected override Task ExecuteCoreAsync(object parameter, CancellationToken cancellationToken)
+			{
+				this.viewModel.Categories.Remove(this.viewModel.SelectedCategory ?? throw new InvalidOperationException("No category is selected."));
+				this.viewModel.SelectedCategory = null;
+				return Task.CompletedTask;
+			}
+
+			private void ViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+			{
+				if (e.PropertyName == nameof(CategoriesPanelViewModel.SelectedCategory))
+				{
+					this.OnCanExecuteChanged();
+				}
+			}
 		}
 	}
 }
---
+++ test/Nerdbank.MoneyManagement.Tests/ViewModels/CategoriesPanelViewModelTests.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the Ms-PL license. See LICENSE.txt file in the project root for full license information.
+
+using Nerdbank.MoneyManagement.ViewModels;
+using Xunit;
+using Xunit.Abstractions;
+
+public class CategoriesPanelViewModelTests : TestBase
+{
+	private CategoriesPanelViewModel viewModel = new CategoriesPanelViewModel();
+
+	public CategoriesPanelViewModelTests(ITestOutputHelper logger)
+		: base(logger)
+	{
+	}
+
+	[Fact]
+	public void InitialState()
+	{
+		Assert.Empty(this.viewModel.Categories);
+		Assert.Null(this.viewModel.SelectedCategory);
+	}
+
+	[Fact]
+	public void AddCommand()
+	{
+		Assert.True(this.viewModel.AddCommand.CanExecute(null));
+
+		this.viewModel.AddCommand.Execute(null);
+		CategoryViewModel newCategory = Assert.Single(this.viewModel.Categories);
+		Assert.Same(newCategory, this.viewModel.SelectedCategory);
+		Assert.Equal(string.Empty, newCategory.Name);
+	}
+
+	[Fact]
+	public void DeleteCommand()
+	{
+		Assert.False(this.viewModel.DeleteCommand.CanExecute(null));
+		this.viewModel.Categories.Add(new CategoryViewModel());
+		Assert.False(this.viewModel.DeleteCommand.CanExecute(null));
+		this.viewModel.SelectedCategory = this.viewModel.Categories[0];
+		Assert.True(this.viewModel.DeleteCommand.CanExecute(null));
+
+		this.viewModel.DeleteCommand.Execute(null);
+		Assert.Empty(this.viewModel.Categories);
+		Assert.Null(this.viewModel.SelectedCategory);
+	}
+}
