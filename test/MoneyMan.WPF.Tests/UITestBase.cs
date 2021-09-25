1fa567c9b48783e81d804e522600e7ca53d43110
Andrew Arnott <andrewarnott@live.com>
2021-09-25 15:13:58 -0600

    Add UI-based tests to verify data binding

---
+++ test/MoneyMan.WPF.Tests/UITestBase.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the Ms-PL license. See LICENSE.txt file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoneyMan;
+using Xunit;
+using Xunit.Abstractions;
+
+public abstract class UITestBase : MoneyTestBase
+{
+	public UITestBase(ITestOutputHelper logger)
+		: base(logger)
+	{
+		this.TraceListener = new XunitTraceListener(logger);
+
+		this.Window = new MainWindow
+		{
+			ReopenLastFile = false,
+		};
+
+		PresentationTraceSources.Refresh();
+		PresentationTraceSources.DataBindingSource.Switch.Level = SourceLevels.Warning;
+		PresentationTraceSources.DataBindingSource.Listeners.Add(this.TraceListener);
+
+		this.Window.ReplaceViewModel(this.DocumentViewModel);
+		if (VisibleWindow)
+		{
+			this.Window.Show();
+		}
+	}
+
+	protected MainWindow Window { get; }
+
+	protected XunitTraceListener TraceListener { get; }
+
+	private static bool VisibleWindow => Debugger.IsAttached;
+
+	protected override void Dispose(bool disposing)
+	{
+		if (disposing)
+		{
+			this.Logger.WriteLine("Data-binding trace switch: {0}, {1}", PresentationTraceSources.DataBindingSource.Switch.Level, PresentationTraceSources.DataBindingSource.Listeners.Count);
+			this.Window.Close();
+			PresentationTraceSources.DataBindingSource.Flush();
+			PresentationTraceSources.DataBindingSource.Listeners.Remove(this.TraceListener);
+
+			// Fail the test if any failures were traced.
+			Assert.False(this.TraceListener.HasLoggedErrors, "Errors have been logged.");
+		}
+
+		base.Dispose(disposing);
+	}
+}
