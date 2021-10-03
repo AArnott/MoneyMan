1a7eaae85cd57a2994c691c0f859b3ac898f4a0b
Andrew Arnott <andrewarnott@live.com>
2021-10-02 22:53:16 -0600

    Make WPF squirrel aware so we get only one shortcut

--- src/MoneyMan.WPF/AssemblyInfo.cs
+++ src/MoneyMan.WPF/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Andrew Arnott. All rights reserved.
 // Licensed under the Ms-PL license. See LICENSE.txt file in the project root for full license information.
 
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
+[assembly: AssemblyMetadata("SquirrelAwareVersion", "1")]
 [assembly: InternalsVisibleTo("MoneyMan.WPF.Tests")]
