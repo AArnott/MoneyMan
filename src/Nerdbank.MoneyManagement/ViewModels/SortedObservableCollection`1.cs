696be7e5bd4b289f2864870e6c7f2f1bf05f90cd
Andrew Arnott <andrewarnott@live.com>
2021-10-10 19:33:14 -0600

    Add `SortedObservableCollection<T>` with tests

---
+++ src/Nerdbank.MoneyManagement/ViewModels/SortedObservableCollection`1.cs
@@ -0,0 +1,185 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the Ms-PL license. See LICENSE.txt file in the project root for full license information.
+
+namespace Nerdbank.MoneyManagement.ViewModels
+{
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Collections.Specialized;
+	using System.ComponentModel;
+	using System.Diagnostics;
+
+	/// <summary>
+	/// A sorted, observable collection.
+	/// </summary>
+	/// <typeparam name="T">The type of element kept by the collection.</typeparam>
+	/// <remarks>
+	/// <typeparamref name="T"/> should be immutable such that the sort result will never change,
+	/// <em>or</em> it should implement <see cref="INotifyPropertyChanged"/> and raise <see cref="INotifyPropertyChanged.PropertyChanged"/>
+	/// whenever a property that may impact sort order has changed.
+	/// The collection will automatically resort a changed item within the collection anytime this property has changed.
+	/// </remarks>
+	[DebuggerDisplay("Count = {" + nameof(Count) + "}")]
+	public class SortedObservableCollection<T> : ICollection<T>, IEnumerable<T>, IEnumerable, IReadOnlyCollection<T>, IReadOnlyList<T>, ICollection, INotifyCollectionChanged, INotifyPropertyChanged
+	{
+		private static readonly NotifyCollectionChangedEventArgs ResetCollectionChanged = new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset);
+		private static readonly PropertyChangedEventArgs CountPropertyChangedArgs = new(nameof(Count));
+
+		private readonly IComparer<T> comparer;
+
+		[DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+		private readonly List<T> list = new();
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SortedObservableCollection{T}"/> class.
+		/// </summary>
+		/// <param name="comparer">The comparer to use. When <see langword="null"/> the <see cref="Comparer{T}.Default"/> comparer is used.</param>
+		public SortedObservableCollection(IComparer<T>? comparer = null)
+		{
+			this.comparer = comparer ?? Comparer<T>.Default;
+		}
+
+		/// <inheritdoc/>
+		public event NotifyCollectionChangedEventHandler? CollectionChanged;
+
+		/// <inheritdoc/>
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		/// <inheritdoc/>
+		[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+		public int Count => this.list.Count;
+
+		/// <inheritdoc/>
+		[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+		bool ICollection<T>.IsReadOnly => false;
+
+		/// <inheritdoc/>
+		[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+		bool ICollection.IsSynchronized => false;
+
+		/// <inheritdoc/>
+		[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+		object ICollection.SyncRoot => this;
+
+		/// <inheritdoc/>
+		public T this[int index] => this.list[index];
+
+		/// <inheritdoc/>
+		void ICollection.CopyTo(Array array, int index) => ((ICollection)this.list).CopyTo(array, index);
+
+		/// <inheritdoc/>
+		public void CopyTo(T[] array, int arrayIndex) => this.list.CopyTo(array, arrayIndex);
+
+		/// <inheritdoc/>
+		public void Add(T item)
+		{
+			int index = this.list.BinarySearch(item, this.comparer);
+			if (index < 0)
+			{
+				index = ~index;
+			}
+
+			this.list.Insert(index, item);
+			if (item is INotifyPropertyChanged observableItem)
+			{
+				observableItem.PropertyChanged += this.Item_PropertyChanged;
+			}
+
+			this.OnPropertyChanged(nameof(this.Count));
+			if (this.CollectionChanged is object)
+			{
+				this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item, index));
+			}
+		}
+
+		/// <inheritdoc/>
+		public bool Contains(T item) => this.IndexOf(item) >= 0;
+
+		/// <inheritdoc/>
+		public bool Remove(T item)
+		{
+			int index = this.IndexOf(item);
+			if (index >= 0)
+			{
+				if (item is INotifyPropertyChanged observableItem)
+				{
+					observableItem.PropertyChanged -= this.Item_PropertyChanged;
+				}
+
+				this.list.RemoveAt(index);
+				this.OnPropertyChanged(nameof(this.Count));
+				if (this.CollectionChanged is object)
+				{
+					this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, item, index));
+				}
+
+				return true;
+			}
+
+			return false;
+		}
+
+		/// <inheritdoc/>
+		public void Clear()
+		{
+			if (this.list.Count > 0)
+			{
+				foreach (T item in this.list)
+				{
+					if (item is INotifyPropertyChanged observableItem)
+					{
+						observableItem.PropertyChanged -= this.Item_PropertyChanged;
+					}
+				}
+
+				this.list.Clear();
+				this.OnPropertyChanged(nameof(this.Count));
+				this.OnCollectionChanged(ResetCollectionChanged);
+			}
+		}
+
+		/// <summary>
+		/// Returns an enumerator that enumerates through the list.
+		/// </summary>
+		/// <returns>The enumerator.</returns>
+		public List<T>.Enumerator GetEnumerator() => this.list.GetEnumerator();
+
+		/// <inheritdoc/>
+		IEnumerator<T> IEnumerable<T>.GetEnumerator() => this.GetEnumerator();
+
+		/// <inheritdoc/>
+		IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+		/// <summary>
+		/// Raises the <see cref="PropertyChanged"/> event.
+		/// </summary>
+		/// <param name="propertyName">The name of the property that changed.</param>
+		protected void OnPropertyChanged(string propertyName) => this.PropertyChanged?.Invoke(this, propertyName == nameof(this.Count) ? CountPropertyChangedArgs : new PropertyChangedEventArgs(propertyName));
+
+		/// <summary>
+		/// Raises the <see cref="CollectionChanged"/> event.
+		/// </summary>
+		/// <param name="args">The arguments to pass to the handlers.</param>
+		protected void OnCollectionChanged(NotifyCollectionChangedEventArgs args) => this.CollectionChanged?.Invoke(this, args);
+
+		private void Item_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+		{
+			if (sender is T item)
+			{
+				// Consider whether this item should be repositioned in the list.
+				(int OldIndex, int NewIndex) positions = this.list.UpdateSortPosition(item, this.comparer);
+				if (positions.OldIndex != positions.NewIndex && this.CollectionChanged is object)
+				{
+					this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Move, item, positions.NewIndex, positions.OldIndex));
+				}
+			}
+		}
+
+		private int IndexOf(T item)
+		{
+			// PERF: we could use binary search to find this more quickly, but be aware it may find any one of several in a row that sort as equal.
+			return this.list.IndexOf(item);
+		}
+	}
+}
---
+++ test/Nerdbank.MoneyManagement.Tests/ViewModels/SortedObservableCollectionTests.cs
@@ -0,0 +1,296 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the Ms-PL license. See LICENSE.txt file in the project root for full license information.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Microsoft;
+using Nerdbank.MoneyManagement.Tests;
+using Nerdbank.MoneyManagement.ViewModels;
+using Xunit;
+using Xunit.Abstractions;
+
+public class SortedObservableCollectionTests : TestBase
+{
+	private SortedObservableCollection<int> collection = new(new DescendingIntComparer());
+
+	public SortedObservableCollectionTests(ITestOutputHelper logger)
+		: base(logger)
+	{
+	}
+
+	[Fact]
+	public void IsReadOnly() => Assert.False(((ICollection<int>)this.collection).IsReadOnly);
+
+	[Fact]
+	public void IsSynchronized() => Assert.False(((ICollection)this.collection).IsSynchronized);
+
+	[Fact]
+	public void SyncRoot() => Assert.NotNull(((ICollection)this.collection).SyncRoot);
+
+	[Fact]
+	public void Add()
+	{
+		this.collection.Add(5);
+		Assert.Equal(5, Assert.Single(this.collection));
+	}
+
+	[Fact]
+	public void Indexer()
+	{
+		Assert.Throws<ArgumentOutOfRangeException>(() => this.collection[0]);
+		this.collection.Add(3);
+		this.collection.Add(5);
+		Assert.Equal(5, this.collection[0]);
+		Assert.Equal(3, this.collection[1]);
+	}
+
+	[Fact]
+	public void CopyTo()
+	{
+		this.collection.CopyTo(Array.Empty<int>(), 0);
+		int[] target = new int[4];
+		this.collection.Add(1);
+		this.collection.Add(3);
+		this.collection.Add(5);
+		this.collection.CopyTo(target, 1);
+		Assert.Equal(new[] { 0, 5, 3, 1 }, target);
+	}
+
+	[Fact]
+	public void CopyTo_NonGeneric()
+	{
+		ICollection collection = this.collection;
+		collection.CopyTo(Array.Empty<int>(), 0);
+		int[] target = new int[4];
+		this.collection.Add(1);
+		this.collection.Add(3);
+		this.collection.Add(5);
+		collection.CopyTo(target, 1);
+		Assert.Equal(new[] { 0, 5, 3, 1 }, target);
+	}
+
+	[Fact]
+	public void Contains()
+	{
+#pragma warning disable xUnit2017 // Do not use Contains() to check if a value exists in a collection
+		Assert.False(this.collection.Contains(1));
+		this.collection.Add(1);
+		Assert.True(this.collection.Contains(1));
+#pragma warning restore xUnit2017 // Do not use Contains() to check if a value exists in a collection
+	}
+
+	[Fact]
+	public void Remove()
+	{
+		Assert.False(this.collection.Remove(1));
+		this.collection.Add(3);
+		this.collection.Add(5);
+		Assert.True(this.collection.Remove(3));
+		Assert.True(this.collection.Remove(5));
+		Assert.False(this.collection.Remove(5));
+	}
+
+	[Fact]
+	public void Clear()
+	{
+		this.collection.Clear();
+		this.collection.Add(3);
+		this.collection.Clear();
+		Assert.Empty(this.collection);
+	}
+
+	[Fact]
+	public void Add_RaisesPropertyChanged()
+	{
+		TestUtilities.AssertPropertyChangedEvent(this.collection, () => this.collection.Add(5), nameof(this.collection.Count));
+	}
+
+	[Fact]
+	public void Remove_RaisesPropertyChanged()
+	{
+		this.collection.Add(3);
+		TestUtilities.AssertPropertyChangedEvent(this.collection, () => Assert.True(this.collection.Remove(3)), nameof(this.collection.Count));
+		TestUtilities.AssertPropertyChangedEvent(this.collection, () => Assert.False(this.collection.Remove(3)), invertExpectation: true, nameof(this.collection.Count));
+	}
+
+	[Fact]
+	public void Clear_RaisesPropertyChanged()
+	{
+		TestUtilities.AssertPropertyChangedEvent(this.collection, () => this.collection.Clear(), invertExpectation: true, nameof(this.collection.Count));
+		this.collection.Add(5);
+		TestUtilities.AssertPropertyChangedEvent(this.collection, () => this.collection.Clear(), nameof(this.collection.Count));
+	}
+
+	[Fact]
+	public void Add_RaisesCollectionChanged()
+	{
+		NotifyCollectionChangedEventArgs args = TestUtilities.AssertCollectionChangedEvent(this.collection, () => this.collection.Add(5));
+		Assert.Equal(NotifyCollectionChangedAction.Add, args.Action);
+		Assert.Null(args.OldItems);
+		Assert.Equal(new[] { 5 }, args.NewItems);
+		Assert.Equal(0, args.NewStartingIndex);
+
+		args = TestUtilities.AssertCollectionChangedEvent(this.collection, () => this.collection.Add(10));
+		Assert.Equal(NotifyCollectionChangedAction.Add, args.Action);
+		Assert.Null(args.OldItems);
+		Assert.Equal(new[] { 10 }, args.NewItems);
+		Assert.Equal(0, args.NewStartingIndex);
+
+		args = TestUtilities.AssertCollectionChangedEvent(this.collection, () => this.collection.Add(1));
+		Assert.Equal(NotifyCollectionChangedAction.Add, args.Action);
+		Assert.Null(args.OldItems);
+		Assert.Equal(new[] { 1 }, args.NewItems);
+		Assert.Equal(2, args.NewStartingIndex);
+	}
+
+	[Fact]
+	public void Remove_RaisesCollectionChanged()
+	{
+		TestUtilities.AssertNoCollectionChangedEvent(this.collection, () => this.collection.Remove(3));
+
+		this.collection.Add(3);
+		this.collection.Add(5);
+		NotifyCollectionChangedEventArgs args = TestUtilities.AssertCollectionChangedEvent(this.collection, () => this.collection.Remove(3));
+		Assert.Equal(NotifyCollectionChangedAction.Remove, args.Action);
+		Assert.Null(args.NewItems);
+		Assert.Equal(new[] { 3 }, args.OldItems);
+		Assert.Equal(1, args.OldStartingIndex);
+	}
+
+	[Fact]
+	public void Clear_RaisesCollectionChanged()
+	{
+		TestUtilities.AssertNoCollectionChangedEvent(this.collection, () => this.collection.Clear());
+		this.collection.Add(5);
+		NotifyCollectionChangedEventArgs args = TestUtilities.AssertCollectionChangedEvent(this.collection, () => this.collection.Clear());
+		Assert.Equal(NotifyCollectionChangedAction.Reset, args.Action);
+		Assert.Null(args.NewItems);
+		Assert.Null(args.OldItems);
+	}
+
+	[Fact]
+	public void ItemChangesResortCollection()
+	{
+		SortedObservableCollection<ObservableMutableClass> collection = new(new MutableClassComparer());
+		ObservableMutableClass a = new(1);
+		ObservableMutableClass b = new(2);
+		collection.Add(a);
+		collection.Add(b);
+
+		NotifyCollectionChangedEventArgs args = TestUtilities.AssertCollectionChangedEvent(collection, () => a.Value = 3);
+		Assert.Equal(NotifyCollectionChangedAction.Move, args.Action);
+		Assert.Equal(0, args.OldStartingIndex);
+		Assert.Equal(1, args.NewStartingIndex);
+		Assert.Same(a, Assert.Single(args.OldItems));
+		Assert.Same(a, Assert.Single(args.NewItems));
+		Assert.Equal(new[] { b, a }, collection);
+	}
+
+	[Fact]
+	public void Remove_ReleasesHandlerReference()
+	{
+		SortedObservableCollection<ObservableMutableClass> collection = new(new MutableClassComparer());
+		ObservableMutableClass a = new(1);
+		collection.Add(a);
+		Assert.Equal(1, a.HandlersCount);
+		collection.Remove(a);
+		Assert.Equal(0, a.HandlersCount);
+	}
+
+	[Fact]
+	public void Clear_ReleasesHandlerReference()
+	{
+		SortedObservableCollection<ObservableMutableClass> collection = new(new MutableClassComparer());
+		ObservableMutableClass a = new(1);
+		collection.Add(a);
+		Assert.Equal(1, a.HandlersCount);
+		collection.Clear();
+		Assert.Equal(0, a.HandlersCount);
+	}
+
+	[Fact]
+	public void GetEnumerator_GenericInterface()
+	{
+		this.collection.Add(3);
+		this.collection.Add(5);
+		IEnumerable<int> enumerable = this.collection;
+		Assert.Equal(new[] { 5, 3 }, enumerable.ToArray());
+	}
+
+	[Fact]
+	public void GetEnumerator_NonGenericInterface()
+	{
+		this.collection.Add(3);
+		this.collection.Add(5);
+		IEnumerable enumerable = this.collection;
+		IEnumerator enumerator = enumerable.GetEnumerator();
+		Assert.True(enumerator.MoveNext());
+		Assert.Equal(5, enumerator.Current);
+		Assert.True(enumerator.MoveNext());
+		Assert.Equal(3, enumerator.Current);
+		Assert.False(enumerator.MoveNext());
+	}
+
+	[Fact]
+	public void Count()
+	{
+#pragma warning disable xUnit2013 // Do not use equality check to check for collection size.
+		Assert.Equal(0, this.collection.Count);
+		this.collection.Add(3);
+		Assert.Equal(1, this.collection.Count);
+#pragma warning restore xUnit2013 // Do not use equality check to check for collection size.
+	}
+
+	[Fact]
+	public void DefaultComparer()
+	{
+		this.collection = new();
+		this.collection.Add(3);
+		this.collection.Add(5);
+		this.collection.Add(1);
+		Assert.Equal(new[] { 1, 3, 5 }, this.collection);
+	}
+
+	private class DescendingIntComparer : IComparer<int>
+	{
+		public int Compare(int x, int y) => -x.CompareTo(y);
+	}
+
+	private class ObservableMutableClass : INotifyPropertyChanged
+	{
+		private int value;
+
+		internal ObservableMutableClass(int value)
+		{
+			this.value = value;
+		}
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		public int Value
+		{
+			get => this.value;
+			set
+			{
+				this.value = value;
+				this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.Value)));
+			}
+		}
+
+		internal int HandlersCount => this.PropertyChanged?.GetInvocationList().Length ?? 0;
+	}
+
+	private class MutableClassComparer : IComparer<ObservableMutableClass>
+	{
+		public int Compare(ObservableMutableClass? x, ObservableMutableClass? y)
+		{
+			Assumes.False(x is null || y is null);
+			return x.Value.CompareTo(y.Value);
+		}
+	}
+}
