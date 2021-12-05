7337f4d7dcdc42060052b5b0f5692846f1bb0005
Andrew Arnott <andrewarnott@live.com>
2021-12-05 08:23:18 -0700

    Add UITest to catch some of the new binding failures that can occur

--- test/MoneyMan.WPF.Tests/UITests.cs
+++ test/MoneyMan.WPF.Tests/UITests.cs
@@ -2,6 +2,7 @@
 // Licensed under the Ms-PL license. See LICENSE.txt file in the project root for full license information.
 
 using System.Windows.Data;
+using Nerdbank.MoneyManagement;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -18,5 +19,16 @@ public class UITests : UITestBase
 	{
 		this.DocumentViewModel.BankingPanel.SelectedAccount = this.DocumentViewModel.AccountsPanel.NewBankingAccount("Checking");
 		this.Window.TransactionDataGrid.SelectedItem = CollectionView.NewItemPlaceholder;
+	}
+
+	[WpfFact]
+	public void CreateInvestmentAccount()
+	{
+		this.DocumentViewModel.SelectedViewIndex = Nerdbank.MoneyManagement.ViewModels.DocumentViewModel.SelectableViews.Accounts;
+		this.DocumentViewModel.AccountsPanel.NewBankingAccount();
+		this.DocumentViewModel.AccountsPanel.SelectedAccount!.Name = "Brokerage";
+		this.DocumentViewModel.AccountsPanel.SelectedAccount!.Type = Account.AccountType.Investing;
+		this.DocumentViewModel.SelectedViewIndex = Nerdbank.MoneyManagement.ViewModels.DocumentViewModel.SelectableViews.Banking;
+		this.DocumentViewModel.BankingPanel.SelectedAccount = this.DocumentViewModel.AccountsPanel.SelectedAccount;
 	}
 }
