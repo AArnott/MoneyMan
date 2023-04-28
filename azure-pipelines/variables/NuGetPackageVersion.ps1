ff8fc868627e180a23285abd28caacd9a67be5b4
Andrew Arnott <andrewarnott@live.com>
2023-04-27 19:13:30 -0600

    Fix AzP build break

--- azure-pipelines/variables/NuGetPackageVersion.ps1
+++ azure-pipelines/variables/NuGetPackageVersion.ps1
@@ -1,2 +1,2 @@
 # Just capture the value produced earlier in the pipeline.
-& (& "$PSScriptRoot\..\Get-nbgv.ps1") get-version -v NuGetPackageVersion
+dotnet tool run nbgv get-version -v NuGetPackageVersion
